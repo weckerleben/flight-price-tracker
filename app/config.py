cbdd876422095e42cc8fce54d741e562165705da
William Eckerleben <wgeckero@bdp.com.py>
2024-02-27 19:37:08 -0300

    Update config with new settings and cleanup

--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
-SERVICE_ACCOUNT_FILE = 'resources/gsheets-394012-45373b59d14d.json'
+import os
+from dotenv import load_dotenv
+
+# Carga las variables de entorno del archivo .env
+load_dotenv()
+
+SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SPREADSHEET_ID = '1NfWHYqeQ0K6Wj8O6m6tjeiC4oRJyQl9S7TJ_p5eG_OQ'
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 ONE_WAY = 'one_way'
 ROUND_TRIP = 'round_trip'
-CYCLE_SLEEP_MIN = 5
-SEARCH_SLEEP_SEC = 7
-MAX_SEARCHES_BEFORE_RESTART = 24
-DATABASE_URI = 'postgresql+psycopg2://admin:1234@localhost/best_price_scraping'
+CYCLE_SLEEP_MIN = int(os.getenv('CYCLE_SLEEP_MIN', 5))
+SEARCH_SLEEP_SEC = int(os.getenv('SEARCH_SLEEP_SEC', 7))
+MAX_SEARCHES_BEFORE_RESTART = int(os.getenv('MAX_SEARCHES_BEFORE_RESTART', 24))
+DATABASE_URI = os.getenv('DATABASE_URI')
